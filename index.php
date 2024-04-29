6ee074dc1bbd5dbc8e835d65fb0f5b2c1dc43094
Dominik <dev@borchert.me>
2024-04-29 08:35:59 +0200

    feat: add php code sniffer and beautifier

--- index.php
+++ index.php
@@ -11,19 +11,18 @@
 	<header class="hero">
 		<div class="container typography">
 			<?php
-			function wrapWordsInSpan($text)
-			{
-				$words = explode(' ', $text);
-				foreach ($words as $word) {
-					$word = trim($word);
+			function wrap_words_in_span( string $text ) {
+				$words = explode( ' ', $text );
+				foreach ( $words as $word ) {
+					$word = trim( $word );
 					echo "<span><span>$word</span></span>";
 				}
 			}
 			?>
 			<h1>
 				<?php
-				$text = "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aperiam reiciendis molestias aliquid facere earum magni suscipit consequatur fugit non aspernatur.";
-				wrapWordsInSpan($text);
+				$text = 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aperiam reiciendis molestias aliquid facere earum magni suscipit consequatur fugit non aspernatur.';
+				wrap_words_in_span( $text );
 				?>
 			</h1>
 		</div>
