8f6abdcc12e0bb23002729182c7d3bb2ccdf64c6
Dominik <dev@borchert.me>
2024-05-14 22:50:37 +0200

    feat: footer cleanup

--- footer.php
+++ footer.php
@@ -11,21 +11,7 @@
 
 ?>
 
-	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'rustikal' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'rustikal' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'rustikal' ), 'rustikal', '<a href="http://underscores.me/">Dominik Borchert</a>' );
-				?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
+	<footer></footer>
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
